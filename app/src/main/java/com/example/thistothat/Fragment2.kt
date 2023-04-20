2e933bb8fd86eae611c82179f2062f5aafa217ac
Danne95 <dan.nereu@gmail.com>
2023-04-20 18:49:58 +0300

    on and off star icon bit of tidiness.

--- app/src/main/java/com/example/thistothat/Fragment2.kt
+++ app/src/main/java/com/example/thistothat/Fragment2.kt
@@ -1,6 +1,6 @@
 package com.example.thistothat
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,21 +13,55 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import java.io.InputStream
+import java.io.OutputStream
 
 private var relativeLayouts = mutableListOf<RelativeLayout>()
-private var favorite_status = mutableListOf<Int>()
+private var values_matrix = mutableListOf<MutableList<String>>()
 
 class Fragment2 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val inputStream: InputStream = context?.assets?.open("values.csv")as InputStream
+        val lines: List<String> = inputStream.bufferedReader().readLines()
+        val inflater = LayoutInflater.from(context)
+        val parentLayout = view?.findViewById<LinearLayout>(R.id.frag2_linearLayout)
 
-        readValues()
+        lines.forEach {line ->
+            val fields = line.split(",").toMutableList()
+            val relativeLayout = inflater.inflate(R.layout.list_item_template, null) as RelativeLayout
+            val starButton = relativeLayout.findViewById<ImageButton>(R.id.template1_imageButton)
+            val textButton = relativeLayout.findViewById<Button>(R.id.template1_button)
+
+            relativeLayout.id = fields[1].toInt()
+            starButton.id = fields[1].toInt()
+            textButton.id = fields[1].toInt()
+
+            // Add the newly created RelativeLayout object to the relativeLayouts list
+            relativeLayouts.add(relativeLayout)
+            values_matrix.add(fields)
+            parentLayout?.addView(relativeLayout)
+
+            // set buttons from values file
+            when(fields[0].toInt()){
+                1 -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_on))
+                else -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_off))
+            }
+            val title = fields[2] + " to " + fields[3]
+            textButton.text = title
+
+            // set listeners
+            starButton.setOnClickListener{
+                Toast.makeText(requireContext(), "You clicked me, star ID: ${starButton.id}", Toast.LENGTH_SHORT).show()
+                starButtonInteraction(starButton)
+            }
+            textButton.setOnClickListener{
+                Toast.makeText(requireContext(), "You clicked me, text ID: ${textButton.id}", Toast.LENGTH_SHORT).show()
+                textButtonInteruction(textButton)
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_page2, container, false)
     }
@@ -42,43 +76,28 @@ class Fragment2 : Fragment() {
         }
     }
 
-    @SuppressLint("InflateParams")
-    private fun readValues(){
-        val inputStream: InputStream = resources.openRawResource(R.raw.values)
-        val lines: List<String> = inputStream.bufferedReader().readLines()
-        val inflater = LayoutInflater.from(context)
-        val parentLayout = view?.findViewById<LinearLayout>(R.id.frag2_linearLayout)
-
-        lines.forEach {line ->
-            val fields = line.split(",")
-            val relativeLayout = inflater.inflate(R.layout.list_item_template, null) as RelativeLayout
-            val starButton = relativeLayout.findViewById<ImageButton>(R.id.template1_imageButton)
-            val textButton = relativeLayout.findViewById<Button>(R.id.template1_button)
-
-            relativeLayout.id = fields[1].toInt()
-            starButton.id = fields[1].toInt()
-            textButton.id = fields[1].toInt()
-
-            // Add the newly created RelativeLayout object to the relativeLayouts list
-            relativeLayouts.add(relativeLayout)
-            favorite_status.add(fields[0].toInt())
-            parentLayout?.addView(relativeLayout)
-
-            // set buttons from values file
-            when(fields[0].toInt()){
-                1 -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_on))
-                else -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_off))
+    fun starButtonInteraction(starButton: ImageButton){
+        when(values_matrix[starButton.id][1].toInt()){
+            // favorited
+            1 -> {
+                // change to unckecked (hollow star)
+                starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(), android.R.drawable.btn_star_big_off))
+                // change value matrix to unfavorited
+                values_matrix[starButton.id][1] = 0.toString()
+                // change data file to unfavorited
+                val newLine = values_matrix[starButton.id].joinToString(",")
             }
-            val title = fields[2] + " to " + fields[3]
-            textButton.text = title
+            else -> {
+                starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(), android.R.drawable.btn_star_big_on))
+                values_matrix[starButton.id][1] = 1.toString()
+                val newLine = values_matrix[starButton.id].joinToString(",")
+            }
+        }
 
-            // setting listeners
-            starButton.setOnClickListener{
-                Toast.makeText(requireContext(), "You clicked me, star ID: ${starButton.id}", Toast.LENGTH_SHORT).show()
-            }
-            textButton.setOnClickListener{
-                Toast.makeText(requireContext(), "You clicked me, text ID: ${textButton.id}", Toast.LENGTH_SHORT).show()
-            }
-       }
     }
+
+    fun textButtonInteruction(textButton: Button){
+
+    }
+    //@SuppressLint("InflateParams")
 }
