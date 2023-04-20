102409aab759e42ed9aa745bf4ff24a69c61b9a7
Danne95 <dan.nereu@gmail.com>
2023-04-20 14:27:02 +0300

    each row widgets has their corresponding correct ids. both widgets get their onClickListener. OCL print a little message confirming ids

--- app/src/main/java/com/example/thistothat/Fragment2.kt
+++ app/src/main/java/com/example/thistothat/Fragment2.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import java.io.InputStream
@@ -51,27 +52,33 @@ class Fragment2 : Fragment() {
         lines.forEach {line ->
             val fields = line.split(",")
             val relativeLayout = inflater.inflate(R.layout.list_item_template, null) as RelativeLayout
+            val starButton = relativeLayout.findViewById<ImageButton>(R.id.template1_imageButton)
+            val textButton = relativeLayout.findViewById<Button>(R.id.template1_button)
 
             relativeLayout.id = fields[1].toInt()
+            starButton.id = fields[1].toInt()
+            textButton.id = fields[1].toInt()
 
             // Add the newly created RelativeLayout object to the relativeLayouts list
             relativeLayouts.add(relativeLayout)
             favorite_status.add(fields[0].toInt())
             parentLayout?.addView(relativeLayout)
 
-            // Find the imageButton and Button views inside the RelativeLayout object
-            val imageButton = relativeLayout.findViewById<ImageButton>(R.id.template1_imageButton)
-            val button = relativeLayout.findViewById<Button>(R.id.template1_button)
-
+            // set buttons from values file
             when(fields[0].toInt()){
-                1 -> imageButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_on))
-                else -> imageButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_off))
+                1 -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_on))
+                else -> starButton.setImageDrawable(ContextCompat.getDrawable(requireContext(),android.R.drawable.btn_star_big_off))
             }
-
-
-
             val title = fields[2] + " to " + fields[3]
-            button.text = title
+            textButton.text = title
+
+            // setting listeners
+            starButton.setOnClickListener{
+                Toast.makeText(requireContext(), "You clicked me, star ID: ${starButton.id}", Toast.LENGTH_SHORT).show()
+            }
+            textButton.setOnClickListener{
+                Toast.makeText(requireContext(), "You clicked me, text ID: ${textButton.id}", Toast.LENGTH_SHORT).show()
+            }
        }
     }
 }
