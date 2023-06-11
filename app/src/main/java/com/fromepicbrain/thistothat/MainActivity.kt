7ce5d4d64d6db6118beddeb70af9db600a91abd6
Danne95 <dan.nereu@gmail.com>
2023-06-11 14:47:34 +0300

    release version ! 1.0.0 (actually its 1.0.1, some package fixes were missed) change com.example.thstothat package to com.fromepicbrain.thistothat

---
+++ app/src/main/java/com/fromepicbrain/thistothat/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.fromepicbrain.thistothat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // create
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // get elements
+        val tabLayout : TabLayout = findViewById(R.id.tabs)
+        val viewPager2 : ViewPager2 = findViewById(R.id.view_pager)
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager2.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+            when (position){
+                0 -> tab.text = "All"
+                1 -> tab.text = "Favorites"
+                else -> throw IllegalArgumentException("Invalid position CODE: 001")
+            }
+        }.attach()
+    }
+}
---
+++ app/src/main/java/com/fromepicbrain/thistothat/ViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.fromepicbrain.thistothat
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun createFragment(position: Int): Fragment = when (position){
+            0 -> Fragment2()
+            1 -> Fragment1()
+            else -> throw IllegalArgumentException("Invalid position CODE: 002")
+        }
+
+    override fun getItemCount(): Int { return 2}
+}
